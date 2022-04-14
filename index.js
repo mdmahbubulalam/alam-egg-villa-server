1cd149b65e08df9e119c14e9f60749cef5fdf37b
Mohammad Mahbubul Alam <alam.diu13@gmail.com>
2022-04-13 21:53:31 -0700

    add firebase admin credential

--- index.js
+++ index.js
@@ -24,7 +24,20 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./configs/alam-egg-villa-firebase-adminsdk-mp4gw-c6dbc3fb8e.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(
+    {
+      "type": process.env.FIREBASE_TYPE,
+      "project_id": process.env.FIREBASE_PROJECT_ID,
+      "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+      "private_key": process.env.FIREBASE_PRIVATE_KEY,
+      "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+      "client_id": process.env.FIREBASE_CLIENT_ID,
+      "auth_uri": process.env.FIREBASE_AUTH_URI,
+      "token_uri": process.env.FIREBASE_TOKEN_URI,
+      "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+      "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+    }
+  )
 });
 
 
